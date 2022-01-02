a139ce1847ac095f536562a3be0c091373556ae3
adam <adam@harpur.io>
2022-01-02 10:32:50 +0000

    chore: add ansible pull

--- bootstrap-ansible.sh
+++ bootstrap-ansible.sh
@@ -3,3 +3,6 @@
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update -y
 sudo apt-get install -y curl git software-properties-common ansible
+
+## pull ansible
+# sudo ansible-pull -U https://github.com/admhpr/setup.git
