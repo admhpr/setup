9db15c1a61c4b4707d7bbf7b1d2ebc19c059532a
adam <adam@harpur.io>
2022-01-03 12:03:01 +0000

    change env setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,19 +2,7 @@
 
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update -y
-sudo apt-get install -y curl git software-properties-common ansible
-
-## python setup 
-
-sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
-## pyenv
-
-curl https://pyenv.run | bash
-
-pyenv install 3.10.1
+sudo apt-get install -y curl git software-properties-common build-essential ansible
 
 ## pull ansible
 # sudo ansible-pull -U https://github.com/admhpr/setup.git
